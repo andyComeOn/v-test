27bcd8da0fdd33df50153c1193abf3821c0596cb
zhangbo <1174839512@qq.com>
2018-08-03 16:19:17 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="hello">
+
+		<li style="margin-bottom:15px;">
+			{{info}}
+		</li>
+
+		<div @click="getInfo">点击点击</div>
+	</div>
+</template>
+
+<script>
+import { getCookie, setCookie } from '@/utils/util';
+export default {
+    name: "HelloWorld",
+    data() {
+        return {
+            msg: "Welcome to Your Vue.js App",
+            info: ""
+        };
+    },
+    methods: {
+        getInfo() {
+            // 情况一
+            // let tl = getCookie("userInfoTel");
+            // if(tl){
+            // 	this.info = "yes";
+            // }
+
+            // 情况二
+            if (getCookie("userInfoTel")) {
+                this.info = "yes";
+            }
+        }
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+    font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
---
+++ src/utils/util.js
@@ -0,0 +1,13 @@
+export function setCookie(name, value) {
+	var Days = 30;
+	var exp = new Date();
+	exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+	document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+
+
+// 获取cookie
+export function getCookie(name) {
+	var v = window.document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+	return v ? v[2] : null;
+}
